73fcc4ccc876a51f1e249afbd6d38f471cc883f6
Rocco Giovinazzo <giovinazzorocco@hotmail.it>
2017-08-22 17:52:48 +0200

    4 commit

--- smartBot/settings.py
+++ smartBot/settings.py
@@ -38,9 +38,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-
-GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = "{}/libgeos_c.so".format(environ.get('GEOS_LIBRARY_PATH'))
+GDAL_LIBRARY_PATH = "{}/libgdal.so".format(environ.get('GDAL_LIBRARY_PATH'))
+PROJ4_LIBRARY_PATH = "{}/libproj.so".format(environ.get('PROJ4_LIBRARY_PATH'))
 
 # Application definition11
 
